4b1db9bf0cd0bf0f2e70329448c2054dfb076294
jiangche <baishihaojc@163.com>
2017-11-21 17:56:22 +0800

    [fix] 执行时无关 pwd 位置

--- vpntester.sh
+++ vpntester.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 hosts=(j0.0bad.com j1.0bad.com j2.0bad.com j3.0bad.com j4.0bad.com j5.0bad.com hk.0bad.com us0.0bad.com us1.0bad.com us2.0bad.com us2.0bad.com us3.0bad.com us4.0bad.com us5.0bad.com)
 pids=()
@@ -8,7 +9,7 @@ echo -e "开始 vpnso 相关线路测试..."
 
 for host in ${hosts[*]}
 do
-  ./vpntester_one.sh $host &
+  $dir/vpntester_one.sh $host &
   pid=$!
   pids+=($pid)
 done
