df1696426d64190202f027bfc8f74aaec394dedc
jiangche <baishihaojc@163.com>
2017-11-21 17:30:49 +0800

    [update] 输出每个 ping 测试的最短时间

--- vpntester_one.sh
+++ vpntester_one.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-test_counts=10
-result_path="/tmp/ping_result"
-
 host=$1
+test_counts=10
+result_path="/tmp/ping_result_${host}"
+result_sorted_path="${result_path}_sorted"
 
-ping -c $test_counts $host | grep "bytes from" > "${result_path}_${host}"
-wcResult=($(wc "${result_path}_${host}"))
-lines=${wcResult[0]}
+ping -c $test_counts $host | grep "bytes from" > $result_path
+wc_result=($(wc $result_path))
+lines=${wc_result[0]}
+sort -t " " -k 7 $result_path > $result_sorted_path
+first_line=$(head -n 1 $result_sorted_path)
 result=$((200*$lines/$test_counts % 2 + 100*$lines/$test_counts))
-echo "${host} 测试结果: ${result}%"
+echo "${result}% ${host} - ${first_line}"
